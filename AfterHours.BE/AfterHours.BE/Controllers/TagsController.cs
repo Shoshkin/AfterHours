5f3184cd3e1e39494ba90a1b538478d8b15ab604
Oren Hecht <hecht_oren@yahoo.com>
2017-06-20 02:04:15 +0300

    add tags

---
+++ AfterHours.BE/AfterHours.BE/Controllers/TagsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AfterHours.BE;
+using AfterHours.BE.Models.Db;
+
+namespace AfterHours.BE.Controllers
+{
+    public class TagsController : ApiController
+    {
+        private EventsContext db = new EventsContext();
+
+        // GET: api/Tags
+        public IQueryable<Tag> GetTags()
+        {
+            return db.Tags;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TagExists(int id)
+        {
+            return db.Tags.Count(e => e.TagId == id) > 0;
+        }
+    }
+}
---
+++ AfterHours.BE/AfterHours.BE/Models/Db/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AfterHours.BE.Models.Db
+{
+    public class Tag
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TagId { get; set; }
+
+        public string Value { get; set; }
+    }
+}
