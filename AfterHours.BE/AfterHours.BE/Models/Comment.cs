29c2c0863ac06b21d92ff0d57cbce26d0e40ba9c
Oren Hecht <hecht_oren@yahoo.com>
2017-06-19 16:44:33 +0300

    Add comment model, dbset and controller

---
+++ AfterHours.BE/AfterHours.BE/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AfterHours.BE.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public int EventId { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime CommentTime { get; set; }
+
+        [ForeignKey(nameof(EventId))]
+        public Event Event { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+}
