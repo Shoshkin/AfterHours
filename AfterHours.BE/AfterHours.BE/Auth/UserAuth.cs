ffba1c065e3c41f5a72f9447582bf6afaec0eb84
Shai Kvetnoy <shai.kvetnoy@gmail.com>
2017-06-19 17:31:02 +0300

    added userauth helper functions

---
+++ AfterHours.BE/AfterHours.BE/Auth/UserAuth.cs
@@ -0,0 +1,41 @@
+﻿using AfterHours.BE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace AfterHours.BE.Auth
+{
+    public enum UserAuthResult
+    {
+        UserNotRegistered,
+        PasswordIncorrect,
+        OK
+    }
+
+    public static class UserAuth
+    {
+        public static (string username, string password) ParseAuthorizationHeader(HttpRequestMessage request)
+        {
+            var tmp = request.Headers.Authorization.Parameter.Split(',');
+            return (tmp[0], tmp[1]);
+        }
+
+        public static UserAuthResult IsUserAuth(EventsContext context, string username, string password)
+        {
+            User user = context.Users.First(x => x.Username == username);
+            if (user == null)
+                return UserAuthResult.UserNotRegistered;
+
+            return user.Password == password ? UserAuthResult.OK : UserAuthResult.PasswordIncorrect;
+
+        }
+
+        public static bool IsUserOrganizerOfEvent(EventsContext context, string username, int eventid)
+        {
+            var current_userid = context.Users.First(x => x.Username == username).UserId;
+            return context.Organizers.Any(x => x.EventId == eventid && x.UserId == current_userid);
+        }
+    }
+}
