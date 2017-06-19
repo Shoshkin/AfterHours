919d202542f4bb0ca0a5071be3c99877ee179567
Elad Gildnur <elad2266@gmail.com>
2017-06-19 21:24:31 +0300

    Fix some stuff and add new routes

---
+++ AfterHours.FE/routes/comments.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var rp = require('request-promise');
+var url = require("url");
+var ulrJoin = require("url-join");
+var router = express.Router();
+
+var baseUrl = "Comments";
+
+var makeRequest = function (options) {
+    return rp(options)
+        .then(function (response) {
+            return response;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+};
+module.exports = function (apiUrl) {
+    router.post('/', function (req, res, next) {
+        makeRequest(
+            {
+                method: 'POST',
+                uri: ulrJoin(apiUrl, baseUrl),
+                body: req.body
+            }
+        ).then(function (response) {
+            res.send(response);
+        });
+    });
+
+    router.delete('/:id', function (req, res, next) {
+        makeRequest(
+            {
+                method: 'DELETE',
+                uri: ulrJoin(apiUrl, baseUrl, req.params.id),
+                body: req.body
+            }
+        ).then(function (response) {
+            res.send(response);
+        });
+    });
+    return router;
+};
