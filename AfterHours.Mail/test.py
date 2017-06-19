55a95b445b30a8e9f4b913240a90678dd7072b39
Shai Kvetnoy <shai.kvetnoy@gmail.com>
2017-06-19 23:58:42 +0300

    work on emails.

---
+++ AfterHours.Mail/test.py
@@ -0,0 +1,4 @@
+import common_mail
+
+
+common_mail.send_invitation_message("afterhour.mailservice@gmail.com", 'Rambam123456', ["liad.mbs@gmail.com"], "AfterHour Invitation", "20170101T220000Z", "20170101T230000Z", "Basketball Game", "Come Play!")
