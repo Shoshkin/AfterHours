c39f684d4d9d415861892c0c4ab45232df5848a5
Shai Kvetnoy <shai.kvetnoy@gmail.com>
2017-06-19 15:34:53 +0300

    init BE topshelf + owin project

---
+++ AfterHours.BE/AfterHours.BE/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Web.Http;
+using Topshelf;
+using Topshelf.ServiceConfigurators;
+using TopShelf.Owin;
+
+namespace AfterHours.BE
+{
+    public class AfterHoursBE
+    {
+        public void Start()
+        {
+        }
+        public void Stop()
+        {
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(c =>
+            {
+                c.Service<AfterHoursBE>(service =>
+                {
+                    ServiceConfigurator<AfterHoursBE> s = service;
+                    s.ConstructUsing(() => new AfterHoursBE());
+                    s.WhenStarted(a => a.Start());
+                    s.WhenStopped(a => a.Stop());
+
+                    s.OwinEndpoint(app =>
+                    {
+                        app.Domain = "localhost";
+                        app.Port = 8080;
+                        app.ConfigureHttp(httpConfig =>
+                        {
+                            httpConfig.MapHttpAttributeRoutes();
+                        });
+                    });
+                });
+            });
+        }
+
+    }
+}
