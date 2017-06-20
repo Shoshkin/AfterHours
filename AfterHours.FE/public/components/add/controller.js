5b44a8609d8c76a8912557fbd64d297219f14106
Oren Hecht <hecht_oren@yahoo.com>
2017-06-20 11:09:05 +0300

    replaced tags in add event to ngTags

--- AfterHours.FE/public/components/add/controller.js
+++ AfterHours.FE/public/components/add/controller.js
@@ -5,6 +5,7 @@
     app.$inject = ["$scope", "$state", "ApiComm"];
     app.controller("Add", function ($scope, $state, ApiComm) {
         $scope.postEvent = function() {
+            $scope.event.tags = _.map($scope.event.tags, function(t){return t.text}).join(',');
             ApiComm.post("api/events", $scope.event)
                 .then(function (){
                     $state.go("root.home");
